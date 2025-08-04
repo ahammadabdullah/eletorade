7e8fcd6c43906e68076d2eab40b6dcb32d3ee4bd
Ahammad Abdullah <ahammadabdullahnew@gmail.com>
2025-08-04 23:10:01 +0600

    feat: removed unused vars

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Footer from "@/components/footer/footer";
 import FuelYourBody from "@/components/Home/fuel-your-body";
 import FuelYourGame from "@/components/Home/fuel-your-game";
 import Hero from "@/components/Home/hero";
-import Hydrate from "@/components/Home/hydrate";
+// import Hydrate from "@/components/Home/hydrate";
 import PickYourPower from "@/components/Home/pick-your-power";
 
 export default function Home() {
--- src/components/Home/hydrate.tsx
+++ src/components/Home/hydrate.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import HexButton from "../common/Button";
-import hydrate from "@/assets/images/hydrate.png";
-import hydratebg from "@/assets/images/hydratebg.png";
+// import hydrate from "@/assets/images/hydrate.png";
+// import hydratebg from "@/assets/images/hydratebg.png";
 import hydrateshape from "@/assets/images/hydrateshape.png";
 import Image from "next/image";
 const Hydrate = () => {
